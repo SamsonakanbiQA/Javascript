20dca729ce7730bd3618e839b2cd0525339fa9f9
unknown <akanjiadesola7@gmail.com>
2025-03-27 22:38:01 -0700

    my task

---
+++ Secondassignment.js
@@ -0,0 +1,24 @@
+
+let side1 = 5;
+let side2 = 5;
+let side3 = 7;
+
+
+if (side1 === side2 && side2 === side3) {
+    console.log("Equilateral triangle");
+} else if (side1 === side2 || side1 === side3 || side2 === side3) {
+    console.log("Isosceles triangle");
+} else {
+    console.log("Scalene triangle");
+}
+
+
+let number = 7;
+
+
+if (number % 2 === 0) {
+    console.log("number is even");
+} else {
+    console.log("number is odd");
+}
+
